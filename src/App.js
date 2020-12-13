a3fb3ec331b88199458ee5508d7f75ebe9389479
tkyk.niimura <tkyk.niimura@gmail.com>
2020-12-13 12:24:38 +0900

    最後まで実装

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react-dom";
+import React, {Component, useState} from 'react';
 import {BrowserRouter, Route, Link} from "react-router-dom";
 
 const App = () =>(
@@ -32,20 +32,83 @@ const About =() => (
     </div>
 );
 
-const Friends = () =>(
-    <div>
-        <h2>Friends</h2>
-        <p>フレンズのリストを記載</p>
-        <Route exact path="/friends" component={FriendList} />
-        <Route path="/friends/:id" component={Friend} />
-    </div>
-);
+// const Friends = () =>(
+//     <div>
+//         <h2>Friends</h2>
+//         <p>フレンズのリストを記載</p>
+//         <Route exact path="/friends" component={FriendList} />
+//         <Route path="/friends/:id" component={Friend} />
+//     </div>
+// );
 
-const FriendList = () => (
+/**
+ * React hook版の実装（Componentと同じ実装をhookで再実装)
+ */
+const Friends = () =>{
+    let counts = {};
+    FRIENDS.forEach( friends =>{
+        counts[friends.id] = 0
+    })
+    const [likeCounts, setCount] = useState(counts);
+    
+    const handleVote = (id) => {
+        setCount( {
+            ... likeCounts,
+            [id]: likeCounts[id] +1,
+        });
+    }
+    return (
+        <div>
+            <h2>Friends</h2>
+            <p>フレンズのリストを記載</p>
+            <Route exact path="/friends" render={props => <FriendList handleVote={handleVote} />} />
+            <Route path="/friends/:id" render={props => <Friend match={props.match} history={props.history} votes={likeCounts} /> } />
+        </div>
+    );
+}
+
+// /**
+//  * Componentでstate(like数)を保持する
+//  */
+// class Friends extends Component{
+//     constructor(){
+//         super();
+//         this.state = {};
+//         this.handleVote = this.handleVote.bind(this);
+//     }
+
+//     componentWillMount(){
+//         FRIENDS.forEach(friend =>{
+//             this.setState({
+//                 ...this.state,
+//                 [friend.id] : 0,
+//             });
+//         });
+//     }
+
+//     handleVote(id){
+//         this.setState({
+//             [id]: this.state[id] +1,
+//         })
+//     }
+
+//     render(){
+//         return(
+//             <>
+//                 <h2>Friends</h2>
+//                 <Route exact path="/friends" render={props => <FriendList handleVote={this.handleVote} />} />
+//                 <Route path="/friends/:id" render={props => <Friend match={props.match} history={props.history} votes={this.state} /> } />
+//             </>
+//         )
+//     }
+// }
+
+const FriendList = (props) => (
     <ul>
         {FRIENDS.map( friend => (
             <li key={friend.id}>
                 <Link to={`/friends/${friend.id}`}>{friend.nameJa}</Link>
+                <button onClick={ ()=> props.handleVote(friend.id)}>+</button>
             </li>
         ))}
     </ul>
@@ -54,6 +117,7 @@ const FriendList = () => (
 const Friend = props =>{
     const {id} = props.match.params;
     const friend = friendById(id);
+    const vote = props.votes[id];
 
     if (typeof friend === "undefined"){
         return (
@@ -75,6 +139,7 @@ const Friend = props =>{
                 <p style={contentsStyle}>{friend.nameEn}</p>
                 <p style={contentsStyle}><a style={aStyle} onClick={() => props.history.goBack()}>戻る</a></p>
             </div>
+            <h1>{vote}</h1>
         </div>
     )
 }
